eb2c801d17f2c262342a338a74338f599fe7dcda
Gowsalya <gowsalyag514@gmail.com>
2026-03-17 14:35:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState("");
+
+ const getWeather = () => {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=YOUR_API_KEY&units=metric"
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data); 
+
+      if (data.cod === 200) {
+        setWeather(data.main.temp);
+      } else {
+        setWeather("Error: " + data.message);
+      }
+    });
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="card">
+        <h2>🌦 Weather App</h2>
+
+        <input
+          type="text"
+          placeholder="Enter city"
+          onChange={(e) => setCity(e.target.value)}
+        />
+
+        <button onClick={getWeather}>Search</button>
+
+        <h3>{weather && `Temperature: ${weather} °C`}</h3>
+      </div>
     </div>
   );
 }
